5aaa2623246d747cbbff8cbe52bc0b6d4e8501d6
JGironB <juangironb@gmail.com>
2019-05-18 04:16:51 +0000

    Añadidos controladores (sin acciones aún) y definidos los recursos

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :scores
+  resources :events
+  resources :users 
+  resources :administrators
+  resources :categories
+  resources :comments
+  resources :images
+  resources :interests
 end
