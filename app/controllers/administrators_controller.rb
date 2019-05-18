1d0e098278faa82171f1c9f79c60bb016ca0225a
Diego <datovarh@unal.edu.co>
2019-05-18 01:31:59 -0500

    avance controladores
    se agregaron acciones aun faltan

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -1,2 +1,30 @@
 class AdministratorsController < ApplicationController
+
+  def index
+    @administrators = Administrator.all
+    render json: @administrators
+  end
+
+  def show
+    render json: @administrator
+  end
+
+  def create
+    @administrator = Administrator.new(administrator_params)
+
+    if @administrator.save
+      render json: @administrator, status: :created, location: @administrator
+    else
+      render json: @administrator.errors, status: :unprocessable_entity
+    end
+
+  end
+  def update
+    if @administrator.update(administrator_params)
+      render json: @administrator
+    else
+      render json: @administrator.errors, status: :unprocessable_entity
+    end
+  end
+
 end
