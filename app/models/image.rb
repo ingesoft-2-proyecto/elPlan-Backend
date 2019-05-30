3ca54b85eb05b86c9f96f6098ab317cc8cbc3539
Cristian Tafur <cdtafurd@unal.edu.co>
2019-05-30 23:56:53 +0000

    corregido el problema postman images

--- app/models/image.rb
+++ app/models/image.rb
@@ -11,10 +11,10 @@
 #
 
 class Image < ApplicationRecord
-    
-    #validacion
-    validates(:path, presence: true, length: {maximum:45})
-    
-    #Asociacion polimorfica
-    belongs_to :imageable, polymorphic: true
+   
+   #validacion
+   validates(:path, presence: true, length: {maximum:45})
+   
+   #Asociacion polimorfica
+   belongs_to :imageable, polymorphic: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  surname       :string
+#  born_date     :date
+#  age           :integer
+#  password      :string
+#  latitude      :float
+#  longitude     :float
+#  notifications :boolean
+#  email         :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
