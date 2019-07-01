ef97ffd4c9a9216b0cd2a44c42a6e8c3518d24e3
JGironB <juangironb@gmail.com>
2019-07-01 14:28:49 -0500

    cambios en guardado de imagen de usuario

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,13 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
 
-  attributes :id, :name, :surname, :age, :email, :photo_attachment, :my_path, :my_blob #, :born_date
+  attributes :id, :name, :surname, :age, :email #, :photo_attachment, :my_path, :my_blob, :born_date
 
   has_many :events
 
-  def my_path
+  #def my_path
   #  begin
 
-    ActiveStorage::Blob.service.send(:path_for, object.photo.key)
+   # ActiveStorage::Blob.service.send(:path_for, object.photo.key)
     #active_storage_disk_service = ActiveStorage::Service::DiskService.new(root: Rails.root.to_s + '/storage/')
     #active_storage_disk_service.send(:path_for, object.photo.blob.key)
 
@@ -15,9 +15,9 @@ class UserSerializer < ActiveModel::Serializer
       #rescue Exception1 => e
     #    object.photo_attachment.blob
     #end
-  end
+  #end
 
-  def my_blob
-    object.photo_attachment.blob
-  end
+  #def my_blob
+   # object.photo_attachment.blob
+  #end
 end
